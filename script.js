6d2a60879ce72e2063dfd3564ba4b71f49749114
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2024-04-30 19:16:28 -0600

    Creando y reproduciendo audio

--- script.js
+++ script.js
@@ -4,22 +4,30 @@ const btnEnfoque = document.querySelector('.app__card-button--enfoque');
 const btnLargo = document.querySelector('.app__card-button--largo');
 const banner = document.querySelector('.app__image');
 const title = document.querySelector('.app__title');
-
+const botones = document.querySelectorAll('.app__card-button');
+const btnMusica = document.querySelector('#alternar-musica');
+const musicaLunaRise = new Audio('/sonidos/luna-rise-part-one.mp3');
 btnCorto.addEventListener('click',()=>{
     fnCambiarContexto('descanso-corto');
+    btnCorto.classList.add('active');
 });
 
 btnEnfoque.addEventListener('click',()=>{
     fnCambiarContexto('enfoque');
+    btnEnfoque.classList.add('active');
 });
 
 btnLargo.addEventListener('click',()=>{
     fnCambiarContexto('descanso-largo');
+    btnLargo.classList.add('active');
 });
 
 function fnCambiarContexto(contexto){
     html.setAttribute('data-contexto',contexto);
     banner.setAttribute('src',`/imagenes/${contexto}.png`);
+    botones.forEach((boton)=>{
+        boton.classList.remove('active');
+    });
     switch (contexto) {
         case "descanso-corto":
             title.innerHTML=`
@@ -46,3 +54,11 @@ function fnCambiarContexto(contexto){
     }
 
 }
+
+btnMusica.addEventListener('change',()=>{
+    if(musicaLunaRise.paused){
+        musicaLunaRise.play();
+    }else{
+        musicaLunaRise.pause()
+    }
+})
